386a9db1aa3719049764c1750d9d801c5d155da8
dgerardi <78278648+dgerardi@users.noreply.github.com>
2021-11-17 16:34:00 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,3 +1,20 @@
+  var ros = new ROSLIB.Ros({
+    url : 'ws://localhost:9090'
+  });
+
+  ros.on('connection', function() {
+    console.log('Connected to websocket server.');
+  });
+
+  ros.on('error', function(error) {
+    console.log('Error connecting to websocket server: ', error);
+  });
+
+  ros.on('close', function() {
+    console.log('Connection to websocket server closed.');
+  });
+
+
 function myFunction() {
     document.getElementById("demo").style.color = "red";
 }
