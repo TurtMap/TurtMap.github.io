2bcabe397330c941ce0da485a5d89db82dc55a53
Gavin Uhran <gavinu2001@gmail.com>
2021-12-01 15:42:59 -0500

    Button changes

--- backend.js
+++ backend.js
@@ -1,40 +1,35 @@
-  var ros = new ROSLIB.Ros({
-    url : 'ws://localhost:9090'
-  });
+var ros = new ROSLIB.Ros({
+  url : 'ws://localhost:9090'
+});
 
-  ros.on('connection', function() {
-    console.log('Connected to websocket server.');
-  });
+ros.on('connection', function() {
+  console.log('Connected to websocket server.');
+});
 
-  ros.on('error', function(error) {
-    console.log('Error connecting to websocket server: ', error);
-  });
+ros.on('error', function(error) {
+  console.log('Error connecting to websocket server: ', error);
+});
 
-  ros.on('close', function() {
-    console.log('Connection to websocket server closed.');
-  });
+ros.on('close', function() {
+  console.log('Connection to websocket server closed.');
+});
 
-  var cmdVel = new ROSLIB.Topic({
-  ros : ros,
-  name : '/cmd_vel',
-  messageType : 'geometry_msgs/Twist'
-  });
+var cmdVel = new ROSLIB.Topic({
+ros : ros,
+name : '/cmd_vel',
+messageType : 'geometry_msgs/Twist'
+});
 
-  var twist = new ROSLIB.Message({
-  linear : {
-    x : 0.1,
-    y : 0.2,
-    z : 0.3
-  },
-  angular : {
-    x : -0.1,
-    y : -0.2,
-    z : -0.3
-  }
-  });
-  cmdVel.publish(twist);
-
-
-function myFunction() {
-    document.getElementById("demo").style.color = "red";
+var twist = new ROSLIB.Message({
+linear : {
+  x : 0.1,
+  y : 0.2,
+  z : 0.3
+},
+angular : {
+  x : -0.1,
+  y : -0.2,
+  z : -0.3
 }
+});
+cmdVel.publish(twist);
